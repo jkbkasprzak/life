258b0922a1b50114210e6a8419d6f6dcbb1bc3b3
jkbkasprzak <73919276+jkbkasprzak@users.noreply.github.com>
2026-03-21 00:24:53 +0100

    Use test fixtures

--- life_core/tests/functional/test_simulation.cpp
+++ life_core/tests/functional/test_simulation.cpp
@@ -1,12 +1,37 @@
+#include <cassert>
 #include <gtest/gtest.h>
 
 #include <gol/simulation.h>
 
-// WHEN trying to create simulation
-// THEN operation finishes successfully and simulation handle is returned
-TEST(SimulationTest, CanCreateSimulation) {
+
+class SimulationTest : public ::testing::Test {
+protected:
   gol_simulation simulation = nullptr;
-  gol_result result = gol_simulation_create(&simulation);
-  EXPECT_EQ(result, GOL_RESULT_SUCCESS);
-  EXPECT_NE(simulation, nullptr);
+
+  void SetUp() override {
+    gol_result result = gol_simulation_create(&simulation);
+    ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+    ASSERT_NE(simulation, nullptr);
+  }
+
+  void TearDown() override {
+    gol_result result = gol_simulation_destroy(simulation);
+    ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+  }
+};
+
+// GIVEN valid simulation
+// WHEN trying to proceed to next simulation step
+// THEN simulation step is updated correctly
+TEST_F(SimulationTest, CanMakeStep) {
+  gol_result result = GOL_RESULT_ERROR;
+  gol_size step = 1000;
+  result = gol_simulation_get_step(simulation, &step);
+  ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+  EXPECT_EQ(step, 0);
+  result = gol_simulation_step(simulation);
+  ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+  result = gol_simulation_get_step(simulation, &step);
+  ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+  EXPECT_EQ(step, 1);
 }
