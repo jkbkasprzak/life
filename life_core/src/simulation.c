47f78a6ce829a7faa0c7d25bf67af7823612d258
jkbkasprzak <73919276+jkbkasprzak@users.noreply.github.com>
2026-03-28 12:32:24 +0100

    Add style

--- life_core/src/simulation.c
+++ life_core/src/simulation.c
@@ -1,41 +1,49 @@
 #include "gol/simulation.h"
 
 gol_result gol_simulation_create(gol_simulation *simulation) {
-  return GOL_RESULT_ERROR;
+    return GOL_RESULT_ERROR;
 }
 
 gol_result gol_simulation_step(gol_simulation simulation) {
-  return GOL_RESULT_ERROR;
+    return GOL_RESULT_ERROR;
 }
 
 gol_result gol_simulation_get_step(gol_simulation simulation, gol_size *step) {
-  return GOL_RESULT_ERROR;
+    return GOL_RESULT_ERROR;
 }
 
-gol_result gol_simulation_set_bounds(gol_simulation simulation, const gol_grid_bounds *bounds) {
-  return GOL_RESULT_ERROR;
+gol_result gol_simulation_set_bounds(gol_simulation simulation,
+                                     const gol_grid_bounds *bounds) {
+    return GOL_RESULT_ERROR;
 }
 
-gol_result gol_simulation_get_bounds(gol_simulation simulation, gol_grid_bounds *bounds) {
-  return GOL_RESULT_ERROR;
+gol_result gol_simulation_get_bounds(gol_simulation simulation,
+                                     gol_grid_bounds *bounds) {
+    return GOL_RESULT_ERROR;
 }
 
-gol_result gol_simulation_get_cell(gol_simulation simulation, const gol_grid_position *position, gol_cell *cell) {
-  return GOL_RESULT_ERROR;
+gol_result gol_simulation_get_cell(gol_simulation simulation,
+                                   const gol_grid_position *position,
+                                   gol_cell *cell) {
+    return GOL_RESULT_ERROR;
 }
 
-gol_result gol_simulation_set_cells(gol_simulation simulation, gol_size size, const gol_cell *cells, gol_result *results) {
-  return GOL_RESULT_ERROR;
+gol_result gol_simulation_set_cells(gol_simulation simulation, gol_size size,
+                                    const gol_cell *cells,
+                                    gol_result *results) {
+    return GOL_RESULT_ERROR;
 }
 
-gol_result gol_simulation_get_alive_cells(gol_simulation simulation, gol_size *size, gol_cell *cells) {
-  return GOL_RESULT_ERROR;
+gol_result gol_simulation_get_alive_cells(gol_simulation simulation,
+                                          gol_size *size, gol_cell *cells) {
+    return GOL_RESULT_ERROR;
 }
 
-gol_result gol_simulation_get_updated_cells(gol_simulation simulation, gol_size *size, gol_cell *cells) {
-  return GOL_RESULT_ERROR;
+gol_result gol_simulation_get_updated_cells(gol_simulation simulation,
+                                            gol_size *size, gol_cell *cells) {
+    return GOL_RESULT_ERROR;
 }
 
 gol_result gol_simulation_destroy(gol_simulation simulation) {
-  return GOL_RESULT_ERROR;
+    return GOL_RESULT_ERROR;
 }
--- life_core/tests/functional/test_simulation.cpp
+++ life_core/tests/functional/test_simulation.cpp
@@ -1,37 +1,39 @@
-#include <cassert>
-#include <gtest/gtest.h>
-
 #include <gol/simulation.h>
-
+#include <gtest/gtest.h>
 
 class SimulationTest : public ::testing::Test {
 protected:
-  gol_simulation simulation = nullptr;
+    gol_simulation simulation = nullptr;
 
-  void SetUp() override {
-    gol_result result = gol_simulation_create(&simulation);
-    ASSERT_EQ(result, GOL_RESULT_SUCCESS);
-    ASSERT_NE(simulation, nullptr);
-  }
+    void SetUp() override {
+        gol_result result = gol_simulation_create(&simulation);
+        ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+        ASSERT_NE(simulation, nullptr);
+    }
 
-  void TearDown() override {
-    gol_result result = gol_simulation_destroy(simulation);
-    ASSERT_EQ(result, GOL_RESULT_SUCCESS);
-  }
+    void TearDown() override {
+        gol_result result = gol_simulation_destroy(simulation);
+        ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+    }
 };
 
 // GIVEN valid simulation
 // WHEN trying to proceed to next simulation step
 // THEN simulation step is updated correctly
 TEST_F(SimulationTest, CanMakeStep) {
-  gol_result result = GOL_RESULT_ERROR;
-  gol_size step = 1000;
-  result = gol_simulation_get_step(simulation, &step);
-  ASSERT_EQ(result, GOL_RESULT_SUCCESS);
-  EXPECT_EQ(step, 0);
-  result = gol_simulation_step(simulation);
-  ASSERT_EQ(result, GOL_RESULT_SUCCESS);
-  result = gol_simulation_get_step(simulation, &step);
-  ASSERT_EQ(result, GOL_RESULT_SUCCESS);
-  EXPECT_EQ(step, 1);
+    gol_result result = GOL_RESULT_ERROR;
+    gol_size step = 1000;
+    result = gol_simulation_get_step(simulation, &step);
+    ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+    EXPECT_EQ(step, 0);
+    result = gol_simulation_step(simulation);
+    ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+    result = gol_simulation_get_step(simulation, &step);
+    ASSERT_EQ(result, GOL_RESULT_SUCCESS);
+    EXPECT_EQ(step, 1);
 }
+
+// GIVEN valid simulation
+// WHEN trying to set bounds
+// THEN simulation bounds are updated successfully
+TEST_F(SimulationTest, CanSetBounds) { gol_result result = GOL_RESULT_ERROR; }
