b13b7e028910544bf930b765bbdaa6dc07cf3986
jkbkasprzak <73919276+jkbkasprzak@users.noreply.github.com>
2026-04-19 21:51:38 +0200

    Add version

--- life_core/include/gol/simulation.h
+++ life_core/include/gol/simulation.h
@@ -13,24 +13,31 @@
 /// @{
 
 /// @brief Generic opaque handle.
+/// @since 1.0.0
 typedef void *gol_handle;
 
 /// @brief Handle for simulation resource.
+/// @since 1.0.0
 typedef gol_handle gol_simulation;
 
 /// @brief Position in single grid dimension.
+/// @since 1.0.0
 typedef int32_t gol_grid_scalar;
 
 /// @brief Boolean type.
+/// @since 1.0.0
 typedef int32_t gol_bool;
 
 /// @brief Type representing nonnegative size of collections.
+/// @since 1.0.0
 typedef uintptr_t gol_size;
 
 /// @brief True value
+/// @since 1.0.0
 #define GOL_TRUE 1
 
 /// @brief False value
+/// @since 1.0.0
 #define GOL_FALSE 0
 
 #ifdef __cplusplus
@@ -38,6 +45,7 @@ extern "C" {
 #endif
 
 /// @brief Defines possible operation results.
+/// @since 1.0.0
 typedef enum {
     GOL_RESULT_SUCCESS = 0, ///< Operation succeded
     GOL_RESULT_ERROR = 1,   ///< Operation failed
@@ -46,6 +54,7 @@ typedef enum {
 } gol_result;
 
 /// @brief Lists available simulation properties.
+/// @since 1.0.0
 typedef enum {
     GOL_SIMULATION_PROPERTY_TYPE_FRAME_COUNT =
         1, ///< Property type for @ref gol_simulation_property_frame_count
@@ -55,6 +64,7 @@ typedef enum {
 } gol_simulation_property_type;
 
 /// @brief Statistics collected per frame about access to grid cells.
+/// @since 1.0.0
 typedef struct {
     /// @brief Count of accesses that returned requested cell.
     uint32_t hit_count;
@@ -64,9 +74,11 @@ typedef struct {
 } gol_simulation_property_grid_access_profiling;
 
 /// @brief Count of time steps processed by the simulation.
+/// @since 1.0.0
 typedef uint32_t gol_simulation_property_frame_count;
 
 /// @brief Position in 2D simulation grid.
+/// @since 1.0.0
 typedef struct {
     /// @brief Position in x dimension.
     gol_grid_scalar x;
@@ -75,6 +87,7 @@ typedef struct {
 } gol_grid_position;
 
 /// @brief Cell at certain position that can be either dead or alive.
+/// @since 1.0.0
 typedef struct {
     /// @brief Cell's position.
     gol_grid_position pos;
@@ -83,6 +96,7 @@ typedef struct {
 } gol_cell;
 
 /// @brief Creates new simulation.
+/// @since 1.0.0
 ///
 /// @li Created simulation is empty - all cells are in dead state.
 /// @li Application is responsible for destroying simulation with
@@ -94,6 +108,7 @@ typedef struct {
 gol_result gol_simulation_create(gol_simulation *simulation);
 
 /// @brief Query simulation property.
+/// @since 1.0.0
 ///
 /// See @ref gol_simulation_property_type for available properties.
 /// @li This operation does not mutate simulation state.
@@ -114,6 +129,7 @@ gol_result gol_simulation_query_property(gol_simulation simulation,
                                          gol_size *bytes, void *data);
 
 /// @brief Progress simulation to next time step.
+/// @since 1.0.0
 ///
 /// @li Simulation state is updated according to simulation rules.
 /// @li This operation mutates simulation state.
@@ -125,6 +141,7 @@ gol_result gol_simulation_query_property(gol_simulation simulation,
 gol_result gol_simulation_next_frame(gol_simulation simulation);
 
 /// @brief Query cells at multiple positions.
+/// @since 1.0.0
 ///
 /// @li This operation does not mutate simulation state.
 /// @li Simultaneus threads may call this function with different simulation
@@ -143,6 +160,7 @@ gol_result gol_simulation_query_cells(gol_simulation simulation, gol_size size,
                                       gol_cell *cells);
 
 /// @brief Update multiple simulation cells.
+/// @since 1.0.0
 ///
 /// @li Modyfing same cell multiple times within a single call is allowed.
 /// @li All updates are applied in order of @a cells array.
@@ -158,6 +176,7 @@ gol_result gol_simulation_set_cells(gol_simulation simulation, gol_size size,
                                     const gol_cell *cells);
 
 /// @brief Query all alive cells.
+/// @since 1.0.0
 ///
 /// @li This operation does not mutate simulation state.
 /// @li Simultaneus threads may call this function with different simulation
@@ -173,6 +192,7 @@ gol_result gol_simulation_get_alive_cells(gol_simulation simulation,
                                           gol_size *size, gol_cell *cells);
 
 /// @brief Query cells updated by the simulation in last time step.
+/// @since 1.0.0
 ///
 /// @li This operation does not mutate simulation state.
 /// @li Simultaneus threads may call this function with different simulation
@@ -189,6 +209,7 @@ gol_result gol_simulation_get_updated_cells(gol_simulation simulation,
                                             gol_size *size, gol_cell *cells);
 
 /// @brief Destroys existing simulation.
+/// @since 1.0.0
 ///
 /// @li All resources owned by the simulation are immediately released.
 /// @li Application must not use simulation handle or resources after it was
